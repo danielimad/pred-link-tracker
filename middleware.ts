14f031d2c0b6f9420acd875e37a0707efdb1bec0
0xDanielimad <72716286+danielimad@users.noreply.github.com>
2025-08-11 16:42:47 +0400

    Update middleware.ts
    redirect to thanks (no WhatsApp here)

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+// middleware.ts
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
@@ -6,24 +7,11 @@ export const config = { matcher: ['/((?!_next|api|favicon\\.ico).*)'] };
 export function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname.slice(1);
 
+  // If the path is exactly an 8-hex ID, redirect to the logger page
   if (/^[0-9a-f]{8}$/i.test(path)) {
-    // Add UA-CH hints for the next request(s)
-    const res = NextResponse.rewrite(new URL(`/thanks?id=${path}`, req.url));
-    res.headers.set('Accept-CH',
-      'Sec-CH-UA, Sec-CH-UA-Platform, Sec-CH-UA-Model, Sec-CH-UA-Platform-Version, Sec-CH-UA-Arch, Sec-CH-UA-Bitness, Sec-CH-UA-Full-Version-List'
-    );
-    res.headers.set('Permissions-Policy',
-      'ch-ua=(self), ch-ua-platform=(self), ch-ua-model=(self), ch-ua-platform-version=(self), ch-ua-arch=(self), ch-ua-bitness=(self), ch-ua-full-version-list=(self)'
-    );
-    return res;
+    // Internal redirect to our logger page so we can run the beacon
+    return NextResponse.redirect(new URL(`/thanks?id=${path}`, req.url));
   }
 
-  const res = NextResponse.next();
-  res.headers.set('Accept-CH',
-    'Sec-CH-UA, Sec-CH-UA-Platform, Sec-CH-UA-Model, Sec-CH-UA-Platform-Version, Sec-CH-UA-Arch, Sec-CH-UA-Bitness, Sec-CH-UA-Full-Version-List'
-  );
-  res.headers.set('Permissions-Policy',
-    'ch-ua=(self), ch-ua-platform=(self), ch-ua-model=(self), ch-ua-platform-version=(self), ch-ua-arch=(self), ch-ua-bitness=(self), ch-ua-full-version-list=(self)'
-  );
-  return res;
+  return NextResponse.next();
 }
